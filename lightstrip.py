022f6a2d94b8f7e10d57e2fe8e14899c109d5618
XenonMolecule <michael_ryan_2000@yahoo.com>
2020-05-06 01:35:32 -0400

    Abstracted out lightstrip control

---
+++ lightstrip.py
@@ -0,0 +1,67 @@
+from neopixel import Adafruit_NeoPixel
+
+# Converts RGB to GRB which is needed by the lightstrip
+def Color(red, green, blue, white = 0):
+	"""Convert the provided red, green, blue color to a 24-bit color value.
+	Each color component should be a value 0-255 where 0 is the lowest intensity
+	and 255 is the highest intensity.
+	"""
+	return (white << 24) | (green << 16)| (red << 8) | blue
+
+# Creates an "interface" (not using programming definition) between
+# my code and the neopixel library to set each of the pixels
+# 
+# This creates a level of abstraction that will let my change how I
+# set each pixel in the future should the need for change arise
+class Lightstrip(object):
+	def __init__(self, cfg):
+		nps = cfg['neopixel']
+		self.strip = Adafruit_NeoPixel(nps['led-count'], \
+			nps['led-pin'], nps['led-freq-hz'], nps['led-dma'], \
+			nps['led-invert'], nps['led-brightness'], nps['led-channel'])
+		
+		self.reversed = cfg['custom']['reversed']
+		
+		self.strip.begin()
+	
+	def _cleanup(self):
+		self.strip._cleanup()
+		
+	def show(self):
+		self.strip.show()
+		
+	# Sets the pixel without updating the strip
+	#  Allows reversal of direction of the strip
+	#  Ensures bounded pixel index from [0, numPixels)
+	def setPixel(self, n, color):
+		pixelNum = self.strip.numPixels() - 1 - n if self.reversed else n
+		pixelNum %= self.strip.numPixels()
+		self.strip.setPixelColor(pixelNum, color)
+	
+	# Sets the pixel and immediately updates the lightstrip visually
+	def setPixelUpdate(self, n, color):
+		self.setPixel(n, color)
+		self.show()
+		
+	def setBrightness(self, n):
+		self.strip.setBrightness(n)
+		
+	def getBrightness(self):
+		self.strip.getBrightness()
+		
+	def setReversed(self, rev):
+		self.reversed = rev
+		
+	def getReversed(self):
+		return self.reversed
+		
+	def numPixels(self):
+		return self.strip.numPixels()
+		
+	# The only animation I am baking into the lightstrip class because
+	#  it has pretty universal importance among other animations and
+	#  the runner class too
+	def clear(self):
+		for i in range(self.strip.numPixels()):
+			self.setPixel(i, Color(0,0,0))
+		self.show()
--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 # Arduino Neopixel library by Tony Dicola
 
 import time
-from neopixel import *
+from lightstrip import *
 import yaml
 
 cfg = {}
@@ -14,30 +14,18 @@ cfg = {}
 # Load settings
 with open("config.yaml", "r") as yamlfile:
 	cfg = yaml.safe_load(yamlfile)
-
-# Create alias for Neopixel specific settings
-nps = cfg["neopixel"]
-
+	
 if __name__ == '__main__':
 	# Create NeoPixel object with appropriate configuration
-	strip = Adafruit_NeoPixel(nps['led-count'], nps['led-pin'], \
-		nps['led-freq-hz'], nps['led-dma'], nps['led-invert'], \
-		nps['led-brightness'], nps['led-channel'])
-	
-	# Initialize the library
-	strip.begin()
+	strip = Lightstrip(cfg)
 	
 	try:
 		while True:
 			for i in range(strip.numPixels()):
-				strip.setPixelColor(i, Color(255,0,0))
-				strip.show()
+				strip.setPixelUpdate(i, Color(255,0,0))
 				time.sleep(20/1000.0)
 			for i in range(strip.numPixels()):
-				strip.setPixelColor(i, Color(0,0,0))
-				strip.show()
+				strip.setPixelUpdate(i, Color(0,0,0))
 				time.sleep(20/1000.0)
 	except KeyboardInterrupt:
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, Color(0,0,0))
-		strip.show()
+		strip.clear()
