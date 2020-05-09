ad12fc9dc04c31a1fb6a323c0ceefb1349a87810
Michael Ryan <michael_ryan_2000@yahoo.com>
2020-05-09 01:24:45 -0400

    Setup webpage for preliminary testing with lightstrip

--- app/api/api.py
+++ app/api/api.py
@@ -1,8 +1,14 @@
 import time
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/setcolor', methods=['POST'])
+def set_color():
+    print(request.get_json())
+    return {'success': 'success'}
--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,30 @@
 import React, {useState, useEffect} from 'react';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+function updateColor(red, green, blue) {
+  const body = JSON.stringify({
+    red: red,
+    green: green,
+    blue: blue
+  });
+
+  fetch('/setcolor', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: body})
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+}
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
+  const [redVal, setRedVal] = useState(0);
+  const [blueVal, setBlueVal] = useState(0);
+  const [greenVal, setGreenVal] = useState(0);
 
   useEffect(() => {
     fetch('/time').then(res => res.json()).then(data => {
@@ -10,7 +33,33 @@ function App() {
   }, []);
 
   return (
-      <p>The current time is {currentTime}.</p>
+      <Container>
+        <p>The current time is {currentTime}.</p>
+        <div style={{'height': '250px', 'width': '250px', 'backgroundColor':'rgb(' + redVal + ',' + blueVal + ',' + greenVal + ')'}}/>
+        <br/>
+        <p>
+          <input type={'range'} min={'0'} max={'255'} value={redVal} onChange = {(e) => {
+            setRedVal(parseInt(e.target.value));
+          }}/>
+          {'  Red: ' + redVal}
+        </p>
+        <br/>
+        <p>
+          <input type={'range'} min={'0'} max={'255'} value={blueVal} onChange = {(e) => {
+            setBlueVal(parseInt(e.target.value));
+          }}/>
+          {'  Blue: ' + blueVal}
+        </p>
+        <br/>
+        <p>
+          <input type={'range'} min={'0'} max={'255'} value={greenVal} onChange = {(e) => {
+            setGreenVal(parseInt(e.target.value));
+          }}/>
+          {'  Green: ' + greenVal}
+        </p>
+        <br/>
+        <Button variant="primary" onClick = {() => {updateColor(redVal, greenVal, blueVal)}}>Update</Button>
+      </Container>
   );
 }
 
