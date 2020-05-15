525f8fbfe59716e1d56e5e6a38e7c02a24e3cd2e
XenonMolecule <michael_ryan_2000@yahoo.com>
2020-05-15 01:37:25 -0400

    Configured mongodb on pi

--- app/api/api.py
+++ app/api/api.py
@@ -4,8 +4,11 @@ import multiprocessing
 from flask import Flask
 from flask import request
 from flask_socketio import SocketIO, emit
+from flask_pymongo import PyMongo
 
 app = Flask('api')
+app.config["MONGO_URI"] = "mongodb://localhost:27017/lightdb"
+mongo = PyMongo(app)
 socketio = SocketIO(app)
 settings = None
 lock = None
@@ -24,6 +27,8 @@ def set_color(message):
 @socketio.on('connect')
 def connect():
     print("New Connection")
+    test_col = mongo.db["testcol"]
+    print(test_col.find_one())
     
 @socketio.on('disconnect')
 def disconnect():
@@ -34,6 +39,8 @@ def run_server(settings_link, read_lock):
     global lock
     settings = settings_link
     lock = read_lock
+    #test_col = mongo.db["testcol"]
+    #test_col.insert_one({"test": "testing", "test2": "still testing"})
     socketio.run(app, host='127.0.0.1', port='5000', debug=False, use_reloader=False)
 
 if __name__ == "__main__":
